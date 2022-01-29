d7c349484e234044b3c032388a83635690922cb5
Scott Bergler <commandinghands@gmail.com>
2022-01-29 07:33:49 -0800

    Set up API for token

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use("/login", (req, res) => {
+  console.log("REQUEST", req.body);
+  res.type("application/json");
+  res.send({
+    token: "test123",
+  });
+});
+
+app.listen(8080, () =>
+  console.log("API is running on http://localhost:8080/login")
+);
