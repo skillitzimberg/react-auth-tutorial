e1723254f5706f75859dc88344a80ec056bbac72
Scott Bergler <commandinghands@gmail.com>
2022-01-29 06:45:54 -0800

    Set up starter routes and components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import Dashboard from "../Dashboard/Dashboard";
+import Preferences from "../Preferences/Preferences";
 
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
