f503d0bc9c9d3ccd0dfbcd6ad7a0d76d862ee964
Scott Bergler <commandinghands@gmail.com>
2022-01-29 07:33:23 -0800

    Add token state as condition for route access

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Dashboard from "../Dashboard/Dashboard";
+import Login from "../Login/Login";
 import Preferences from "../Preferences/Preferences";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login />;
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
